9266f0f3df91dae699cc34b40f714b1873670bec
Afthab Hyder <afthabhydernm@gmail.com>
2023-12-04 00:33:03 +0530

    overwritten code

--- dbmsproject.py
+++ dbmsproject.py
@@ -1,3 +1,16 @@
+
+
+None selected
+
+Skip to content
+Using National Institute of Technology Calicut Mail with screen readers
+
+Conversations
+Using 1.1 GB
+Program Policies
+Powered by Google
+Last account activity: 9 minutes ago
+Details
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -522,7 +535,7 @@ def userpayloan():
         flags=Loan.query.filter_by(UserID=uid).all()
         if not flags:
             return jsonify({'message':'User Has No Active Loans'}),401
-        
+        print(lid)
         found=0
         
         for items in flags:
@@ -571,7 +584,7 @@ def userpayloan():
         if loan.Status=='Closed':
             return jsonify({'message':'Loan Payment Successful.Loan Closed'}),201
         
-        return jsonify({'message':'Loan Payent Successful'}),201
+        return jsonify({'message':'Loan Payment Successful'}),201
         
         
         
